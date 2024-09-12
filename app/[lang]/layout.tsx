28a9e8fee7260adb137e86ecf659d4be7648c7f9
Aminjon Ffff <fayzmuhammadamirjoni@gmail.com>
2024-09-12 14:08:23 +0500

    edit seo

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -25,11 +25,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang={params.lang}>
-      <meta name="author" content="Академия программирования для всех" />
-      <meta name="description"
-        content="Курс программирования в IT академии vesto в душанбе , Best Computer Courses in Dushanbe , барномасози дар душанбе бо забони точики , Курсы веб-программирования в Душанбе , научится программированию в 3 месяца" />
-      <meta property="keywords" name="keywords"
-        content="программирование,Обучение программированию с нуля с трудоустройством ,  where to learn programming in dushanbe, онлайн курсы , онлайн курсы программирования в душанбе,научится программированию в душанбе веб,курс программирования в it в душанбе Front-end, Back-end, Трудоустройство в сфере tech,Карьера программиста,Курсы с трудоустройством,Карьера в сфере IT,Карьера программиста за 5 месяца,Курсы React.js,programming in Dushanbe,programming in Tajikistan , ёд гирифтани барномасози дар точикистон" />
       <body className={inter.className}>
         <Header lang={params.lang} />
         <main>{children}</main>
