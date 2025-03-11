ec5a13c1c28d6e850a8ea6b705dbbf33e19d04e1
Aminjon Ffff <fayzmuhammadamirjoni@gmail.com>
2025-03-11 10:14:52 +0500

    edit size text tab

--- app/[lang]/components/ProjectTag.jsx
+++ app/[lang]/components/ProjectTag.jsx
@@ -8,7 +8,7 @@ const ProjectTag = ({ name, onClick, isSelected, tag, icon: Icon }) => {
     : "text-[#ADB7BE] border-slate-600 hover:border-white";
   return (
     <button
-      className={`${buttonStyles} border-b-2 flex gap-1 border-transparent px-6 py-1 text-lg cursor-pointer relative z-30 -mb-[1px]`}
+      className={`${buttonStyles} border-b-2 flex gap-1 border-transparent  px-2 md:px-4 lg:px-6 py-1 text-sm md:text-base xl:text-lg cursor-pointer relative z-30 -mb-[1px]`}
       onClick={() => onClick(tag)}
     >
       <Icon width={20} />
