9698020ce6455508cd4b6e78effb8292bcb7e58a
Davlatzoda Amirjon <129279570+AmirjonFff@users.noreply.github.com>
2026-08-30 13:08:41 +0500

    Change Telegram link to use HTTP protocol

--- app/[lang]/contact/page.tsx
+++ app/[lang]/contact/page.tsx
@@ -46,7 +46,7 @@ async function page({ params: { lang } }: {
                         <Link href="https://www.linkedin.com/in/%D0%B0%D0%BC%D0%B8%D1%80%D0%B4%D0%B6%D0%BE%D0%BD-%D0%B4%D0%B0%D0%B2%D0%BB%D0%B0%D1%82%D0%B7%D0%BE%D0%B4%D0%B0-1a7278304/">
                             <Image className="w-[45px]" src={LiIcon} alt="Linkedin Icon" />
                         </Link>
-                        <Link href="tg://resolve?domain=aking_0102">
+                        <Link target="_blank" href="http://t.me/aking_0102">
                             <Image className="w-[42px]" src={TgIcon} alt="Telegram Icon" />
                         </Link>
                     </div>
